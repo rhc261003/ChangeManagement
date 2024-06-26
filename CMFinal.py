29562162aaa474a3459518fb6f56451876dd988e
rhc261003 <rishi.c@kkcllp.in>
2024-06-26 15:05:13 +0530

    Update CMFinal.py

--- CMFinal.py
+++ CMFinal.py
@@ -1,7 +1,7 @@
-import seaborn as sns
-import matplotlib.pyplot as plt
 import streamlit as st
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 st.title('Change Management')
 
